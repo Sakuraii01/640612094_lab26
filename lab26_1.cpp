7872404ece8a344702fe7248b514d65fc0dfce17
Sakuraii <pearl.k.sakura@gmail.com>
2022-02-18 18:15:25 +0700

    01 complete

--- lab26_1.cpp
+++ lab26_1.cpp
@@ -63,4 +63,44 @@ void List::append(int d){
 	size++;
 }
 
-//Write List::remove() here
+void List::remove(int num){
+	Node *n = root , *current = NULL;
+	for(int i = 0 ; i < num ; i++){
+		current = n;
+		n = n->next;
+	}
+	if(current){
+		current->next = n->next;
+	}else{
+		root = root->next;
+	}
+	
+	delete n;
+}
+
+int main(){
+	List myList = {0,0};
+	myList.append(5);
+	myList.append(7);
+	myList.append(11);
+	myList.append(4);
+	myList.append(12);
+	myList.append(45);
+
+	myList.insert(29,2);
+myList.insert(33,3);
+myList.insert(77,5);
+myList.insert(69,0);
+myList.show();
+cout << "\n"; 
+
+myList.remove(2);
+myList.show();
+cout << "\n"; 
+myList.remove(4);
+myList.show();
+cout << "\n";
+myList.remove(0);
+myList.show();
+cout << "\n";
+}
